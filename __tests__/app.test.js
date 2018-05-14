dbb4a3ece09282ded27d40933eb3882e76344f41
Levi Donaldson <lcdbacanas@gmail.com>
2018-05-13 20:22:16 -0500

    Update app.test.js

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,8 +2,6 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import App from '../src/App';
 
-
-
 it('renders correctly', () => {
   const tree = renderer.create(
     <App/>

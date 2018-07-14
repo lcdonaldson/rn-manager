93c9ed5a6b779db2277b072b1edc374369bf77d2
Levi Donaldson <lcdbacanas@gmail.com>
2018-07-13 19:29:54 -0500

    Update app.test.js

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,13 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import App from '../src/App';
 
+// Snapshot
 it('renders correctly', () => {
   const tree = renderer.create(<App />).toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+// Unit Test
+it(() => {
+  
 });

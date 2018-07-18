65c68106f6ae74878bc8810671f2416a622e8dd3
Levi Donaldson <lcdbacanas@gmail.com>
2018-07-17 21:04:57 -0500

    Update Loader.spec.js

--- __tests__/Loader/Loader.spec.js
+++ __tests__/Loader/Loader.spec.js
@@ -1,5 +1,27 @@
-describe("Loader component", () => {
-  it ("should render correctly", () {
+import React from "react";
+
+import Enzyme, {shallow} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+function setup(type) {
+  const props = {};
+  let enzymeWrapper;
   
+  return {
+    enzymeWrapper
+  }
+}
+
+describe("Loader component", () => {
+  let setupComponent;
+  let wrapper;
+
+  it ("Snapshot Tests", () {
+    setupComponent = setup();
+    wrapper = setupComponent.enzymeWrapper;
+
+    expect(wrapper).toMatchSnapshot();
   })
 })

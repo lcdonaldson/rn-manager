c20b2e92ab1142f6d550ca10b0e11708c2bb4ff8
Levi Donaldson <lcdbacanas@gmail.com>
2019-03-06 21:30:12 -0600

    reconfiguring auth screen

--- src/components/common/Loader/Loader.js
+++ src/components/common/Loader/Loader.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
-import { TouchableOpacity, ActivityIndicator} from 'react-native';
-import styles from "./styles.js";
+import {
+    TouchableOpacity,
+    ActivityIndicator,
+    StyleSheet,
+    View
+} from 'react-native';
 
-class Loader extends React.Component {
+class Loader extends Component {
     render() {
         return (
             <View style={styles.loaderLayout}> 
-                <ActivityIndicator color={'#fff'} size={large}/>
+                <ActivityIndicator color={'#fff'} size="large"/>
                 <TouchableOpacity style={styles.cancelBtn} text={'Cancel'} />  
             </View>        
         )
     }
 }
 
+const styles = StyleSheet.create({
+    loaderLayout: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        // backgroundColor: '#00000060,',
+    },
+    cancelBtn: {
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingLeft: 30,
+        paddingRight: 30,
+        color: '#fff',
+        backgroundColor: "teal",
+    }
+})
+
+export default Loader;
